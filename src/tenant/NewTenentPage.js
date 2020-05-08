f5452486f1f04db8a8ccb5083dcda0616ea01268
sunao_4_git-at-510229950882 <HRp83k2eh7CeqF1Gj+oZyCNN2+Z/XwJVYbw1fob1YIo=>
2020-05-08 14:36:36 +0900

    improve ui by sunao

--- src/tenant/NewTenentPage.js
+++ src/tenant/NewTenentPage.js
@@ -9,6 +9,10 @@ import Button from '@material-ui/core/Button';
 import SaveIcon from '@material-ui/icons/Save';
 
 import Configuration from "../Configuration";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import AssignmentIcon from "@material-ui/icons/Assignment";
+import {TabPanel} from "./TenantProfilePage";
 
 const useStyles = makeStyles((theme) => ({
     functionPanel: {
@@ -25,6 +29,15 @@ const useStyles = makeStyles((theme) => ({
     },
     basicInformationPanel: {
         margin: theme.spacing(1),
+    },
+    tabRoot: {
+        flexGrow: 1,
+        backgroundColor: theme.palette.background.paper,
+        display: 'flex',
+        height: 500,
+    },
+    tabs: {
+        borderRight: `1px solid ${theme.palette.divider}`,
     },
     button: {
         margin: theme.spacing(1),
@@ -48,6 +61,11 @@ export default function NewTenantPage(props) {
         requestAdd(newData);
     };
 
+    const [tabValue, setValue] = React.useState(0);
+
+    const handleChange = (event, newTabValue) => {
+        setValue(newTabValue);
+    };
 
     return (
         <React.Fragment>
@@ -70,51 +88,71 @@ export default function NewTenantPage(props) {
                 </div>
                 <div className={classes.basicInformationPanel} >
                     <FormControl variant="outlined" className={classes.formControl}>
-                        <InputLabel >テナント名</InputLabel>
-                        <TextField name="name" onChange={changePropertyOfNew} id="standard-basic" label="テナント名"  helperText="会社名を入れてください" />
+                        <TextField name="name" onChange={changePropertyOfNew} id="standard-basic"
+                                   label="テナント名"  helperText="会社名を入れてください"
+                                   margin="normal"
+                        />
+                    </FormControl>
+                    <FormControl variant="outlined" className={classes.formControl}>
+                        <TextField name="alias" onChange={changePropertyOfNew}
+                                   id="standard-basic-alias" label="略称"
+                                   helperText="略称を入れてください" />
                     </FormControl>
                 </div>
                 <Divider variant="middle" />
-                <FormControl variant="outlined" className={classes.formControl}>
-                    <InputLabel htmlFor="outlined-age-native-simple">製品</InputLabel>
-                    <Select
-                        native
-                        label="製品"
-                        inputProps={{
-                            name: 'contract.details.0.productMstId',
-                            id: 'outlined-age-native-simple',
-                        }}
-                        onChange={changePropertyOfNew}
+                <div className={classes.tabRoot}>
+                    <Tabs
+                        orientation="vertical"
+                        variant="scrollable"
+                        value={tabValue}
+                        onChange={handleChange}
+                        aria-label="Vertical tabs example"
+                        className={classes.tabs}
                     >
-                        <option value="1" >CJK</option>
-                        <option value="2" >CWS</option>
-                        <option value="4" >CSR</option>
-                        <option value="3" >CTM</option>
-                    </Select>
-                </FormControl>
-                <TextField
-                    id="standard-number"
-                    name="contract.details.0.amount"
-                    label="ライセンス数"
-                    type="number"
-                    InputLabelProps={{
-                        shrink: true,
-                    }}
-                    onChange={changePropertyOfNew}
-                />
-                <Divider variant="middle" />
-                <TextField
-                    id="standard-multiline-static"
-                    multiline
-                    rows={5}
-                    name="contract.remarks"
-                    label="特記事項"
-                    InputLabelProps={{
-                        shrink: true,
-                    }}
-                    onChange={changePropertyOfNew}
-                />
+                        <Tab label="契約内容" {...a11yProps(0)} icon={<AssignmentIcon />} wrapped />
+                    </Tabs>
+                    <TabPanel value={tabValue} index={0}>
+                        <TextField name="contract.remarks" onChange={changePropertyOfNew} id="standard-basic"
+                                   label="契約特記事項"  helperText="契約特記事項" />
+                        <TextField name="contract.infraAnnualIncome" onChange={changePropertyOfNew}
+                                   id="standard-basic" label="インフラ年間予算"  helperText="インフラ年間予算" />
+                        <FormControl variant="outlined" className={classes.formControl}>
+                            <InputLabel htmlFor="outlined-age-native-simple">製品</InputLabel>
+                            <Select
+                                native
+                                label="製品"
+                                inputProps={{
+                                    name: 'contract.details.0.productMstId',
+                                    id: 'outlined-age-native-simple',
+                                }}
+                                onChange={changePropertyOfNew}
+                            >
+                                <option value="1" >CJK</option>
+                                <option value="2" >CWS</option>
+                                <option value="4" >CSR</option>
+                                <option value="3" >CTM</option>
+                            </Select>
+                        </FormControl>
+                        <TextField
+                            id="standard-number"
+                            name="contract.details.0.amount"
+                            label="ライセンス数"
+                            type="number"
+                            InputLabelProps={{
+                                shrink: true,
+                            }}
+                            onChange={changePropertyOfNew}
+                        />
+                    </TabPanel>
+                </div>
+
             </form>
         </React.Fragment>
     )
+}
+function a11yProps(index) {
+    return {
+        id: `vertical-tab-${index}`,
+        'aria-controls': `vertical-tabpanel-${index}`,
+    };
 }
