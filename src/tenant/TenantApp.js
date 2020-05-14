9623efdc3727e3ced3f7c78dbb52657516a2f5bd
kntASK <36790809+kntASK@users.noreply.github.com>
2020-05-14 12:55:01 +0900

    Remove unnecessary margin
    remove SiteFooter and AppContent margin

--- src/tenant/TenantApp.js
+++ src/tenant/TenantApp.js
@@ -1,61 +1,61 @@
 import React from "react";
-import {Route, useRouteMatch , Switch} from "react-router-dom";
+import { Route, useRouteMatch, Switch } from "react-router-dom";
 import NewTenant from "./NewTenant";
 import TenantList from "./TenantList";
 import TenantDetail from "./TenantProfile";
 import Paper from "@material-ui/core/Paper";
-import {makeStyles} from "@material-ui/core/styles";
+import { makeStyles } from "@material-ui/core/styles";
 import PageTitle from "../components/PageTitle";
 
-const useStyles = makeStyles((theme) => ({
-    appHeader: {
-        padding: theme.spacing(1),
-        margin: theme.spacing(2),
-        position:'relative',
-    },
-    appHeaderWrap: {
-        width : "100%",
-        alignItems : "center",
-        display: 'flex',
-    },
-    appTitle :{
-        flexGrow: 1,
-    },
-    appBreadcrumbs :{
-        textAlign: "right",
-    },
-    appContent: {
-        padding: theme.spacing(2),
-        margin: theme.spacing(2),
-        '& .MuiTextField-root': {
-            margin: theme.spacing(1),
-            width: '25ch',
-        },
-    },
+const useStyles = makeStyles(theme => ({
+  appHeader: {
+    padding: theme.spacing(1),
+    margin: theme.spacing(2),
+    position: "relative"
+  },
+  appHeaderWrap: {
+    width: "100%",
+    alignItems: "center",
+    display: "flex"
+  },
+  appTitle: {
+    flexGrow: 1
+  },
+  appBreadcrumbs: {
+    textAlign: "right"
+  },
+  appContent: {
+    padding: theme.spacing(2),
+    margin: theme.spacing(2, 0),
+    "& .MuiTextField-root": {
+      margin: theme.spacing(1),
+      width: "25ch"
+    }
+  }
 }));
 
 export default function TenantApp() {
-    const classes = useStyles();
-    let { path } = useRouteMatch();
+  const classes = useStyles();
+  let { path } = useRouteMatch();
 
-    return (
-        <React.Fragment>
-            <div className={classes.appHeaderWrap}>
-
-                <PageTitle>
-                    テナントプロファイル
-                </PageTitle>
-            </div>
-            <Paper elevation={3} className={classes.appContent}>
-                <Switch>
-                    <Route exact path={path + "/list"} component={TenantList} />
-                    <Route exact path={path + "/add"} component={NewTenant} />
-                    <Route exact path={path + "/profile"} component={TenantDetail} />
-                    <Route exact path={path + "/profile/:tenantId"} component={TenantDetail} />
-                    <Route exact path={path} component={TenantList} />
-                </Switch>
-            </Paper>
-        </React.Fragment>
-
-    );
+  return (
+    <React.Fragment>
+      <div className={classes.appHeaderWrap}>
+        <PageTitle>テナントプロファイル</PageTitle>
+      </div>
+      <Paper elevation={3} className={classes.appContent}>
+        <Switch>
+          <Route exact path={path + "/list"} component={TenantList} />
+          <Route exact path={path + "/add"} component={NewTenant} />
+          <Route exact path={path + "/profile"} component={TenantDetail} />
+          <Route
+            exact
+            path={path + "/profile/:tenantId"}
+            component={TenantDetail}
+          />
+          <Route exact path={path} component={TenantList} />
+        </Switch>
+      </Paper>
+    </React.Fragment>
+  );
 }
